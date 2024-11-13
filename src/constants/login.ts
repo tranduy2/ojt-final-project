c378ed21d3ff3645f1ce09d2d7db3fd230af4287
thien <thien@192.168.0.122>
2024-11-13 23:19:11 +0700

    feat: update login ui

---
+++ src/constants/login.ts
@@ -0,0 +1,14 @@
+import GoogleIC from '../assets/svgs/Google.svg';
+import AppleIC from '../assets/svgs/Apple.svg';
+import FbIC from '../assets/svgs/Fb.svg';
+
+export const authenticationType = [
+  { text: 'Sign in', value: 'sign-in' },
+  { text: 'Register', value: 'register' },
+];
+
+export const thirdMethod = [
+  { logo: GoogleIC, value: 'Google' },
+  { logo: AppleIC, value: 'Apple' },
+  { logo: FbIC, value: 'Facebook' },
+];
